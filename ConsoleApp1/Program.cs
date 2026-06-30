808ce8c1578c4df63436bdf8d3715189d0f6b4ec
iArttt <volk19011@gmail.com>
2026-06-30 14:15:20 +0300

    Empty

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Lesson
+﻿namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello, World!");
         }
     }
 }
